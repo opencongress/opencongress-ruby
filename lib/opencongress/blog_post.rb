4c05a50f58bbee7405a8115c9e7e3161b7f4fb6f
David Shettler <dave@opencongress.org>
2009-03-08 23:09:02 -0400

    fixes

---
+++ lib/opencongress/blog_post.rb
@@ -0,0 +1,18 @@
+
+module OpenCongress
+  
+  class BlogPost < OpenCongressObject
+    
+    attr_accessor :title, :date, :url, :source_url, :excerpt, :source, :average_rating
+    
+    
+    def initialize(params)
+      params.each do |key, value|
+        instance_variable_set("@#{key}", value) if BlogPost.instance_methods.include? key
+      end      
+    end      
+      
+      
+  end
+  
+end
